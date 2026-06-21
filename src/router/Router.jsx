a38a71b4227abcf1b06983d3c79de80a398e5109
Olivier Barontini <olivier.barontini@gmail.com>
2026-06-21 16:43:50 +0200

    Update Router.jsx

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Logement from "../pages/Logement";
